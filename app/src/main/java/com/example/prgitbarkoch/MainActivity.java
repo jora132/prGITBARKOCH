a9310b719e60be63f800ae886cde7137a564a250
Poplavskiy <danielforcontacting@gmail.com>
2022-03-19 14:55:28 +0700

    com2

---
+++ app/src/main/java/com/example/prgitbarkoch/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.prgitbarkoch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//commit 1
+//commit 2
